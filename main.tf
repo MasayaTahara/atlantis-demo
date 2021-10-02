b9c2ff3d1950b9a626d95afa263285193801c837
MasayaTahara <mocoso.0u1@gmail.com>
2021-10-02 11:12:27 +0900

    Add tfstate manager

--- main.tf
+++ main.tf
@@ -3,3 +3,9 @@ module "network" {
   env    = var.env
   app    = "atlantis-demo"
 }
+
+module "tfstate_manager_module" {
+  source = "./modules/tfstate_manager"
+  env    = var.env
+  app    = "atlantis-demo"
+}
